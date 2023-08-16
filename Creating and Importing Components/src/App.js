cfc80ebae404fd70f6ffae69c6533ba9a5b79241
VIJAYADASS <Vijaydass0702@gmail.com>
2023-08-16 17:27:11 +0530

    Creating and Importing Component

---
+++ Creating and Importing Components/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Heading from "./Heading";
+
+function App() {
+  return (
+    <div className="App">
+      <Heading />
+    </div>
+  );
+}
+ 
+export default App;
+
+
