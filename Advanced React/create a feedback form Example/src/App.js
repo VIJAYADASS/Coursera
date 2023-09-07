86fd694ffe36b9d4fabd6672413ac82a103707db
VIJAYADASS <Vijaydass0702@gmail.com>
2023-09-07 20:01:26 +0530

    full set of feedback form

--- Advanced React/create a feedback form Example/src/App.js
+++ Advanced React/create a feedback form Example/src/App.js
@@ -4,10 +4,21 @@ import { useState } from 'react';
 
 function App() {
   const[score , SetScore] = useState("10");
+  const [comment, setComment] = useState("");
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (Number(score) <= 5 && comment.length <= 10)  {
+      alert("please provide a comment explaning why the expericene was poor. ")
+      return;
+    }
+      console.log("Form Submitted!");
+      setComment("");
+      SetScore("10");
+  }
   return (
     <div className="App">
-      <form>
+      <form onSubmit={handleSubmit}>
         <fieldset>
           <h2>Feedback Form</h2>
           <div className='Feild'>
@@ -19,7 +30,11 @@ function App() {
               value={score}
               onChange={e => SetScore(e.target.value)}
             />
-            
+          </div>
+          <div className='Field'>
+            <label>Comment:  </label>
+            <textarea value={comment} onChange={e => setComment(e.target.value)} />
+
           </div>
           <button type='submit' >Submit</button>
         </fieldset>
