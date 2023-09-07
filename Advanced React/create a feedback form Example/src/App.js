6c4c0fc2960e4de8af6bb2f416c9027078477495
VIJAYADASS <Vijaydass0702@gmail.com>
2023-09-07 19:43:32 +0530

    Add the onChange to sect the score

--- Advanced React/create a feedback form Example/src/App.js
+++ Advanced React/create a feedback form Example/src/App.js
@@ -1,15 +1,24 @@
  import React from 'react';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const[score , SetScore] = useState("10");
+
   return (
     <div className="App">
       <form>
         <fieldset>
-          <h2>Feebback Form</h2>
+          <h2>Feedback Form</h2>
           <div className='Feild'>
-            <label>Score: </label>
-            <input type='range' min="0" max="10" />
+            <label>Score: {score}  </label>
+            <input 
+              type='range'  
+              min="0" 
+              max="10" 
+              value={score}
+              onChange={e => SetScore(e.target.value)}
+            />
             
           </div>
           <button type='submit' >Submit</button>
