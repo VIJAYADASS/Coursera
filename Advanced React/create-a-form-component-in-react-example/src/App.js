1377ae4348ad85c606b7c483029bb7358e6f686d
VIJAYADASS <Vijaydass0702@gmail.com>
2023-09-07 18:53:13 +0530

    Uncontrolled form in react
    Add the onSubmit form.  when the page is not reloaded

--- Advanced React/create-a-form-component-in-react-example/src/App.js
+++ Advanced React/create-a-form-component-in-react-example/src/App.js
@@ -1,18 +1,31 @@
- import React from 'react';
+ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [name, setName] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setName("");
+    console.log("Form Submitted!");
+  };
+
   return(
     <div className='App'>
-      <form>
-        <fieldset>
-          <div className='Feild'>
-            <label>Name</label>
-            <input type='text' placeholder='Name' name='name' />
-          </div>
-          <button type='submit'>Submit</button>
-        </fieldset>
-      </form>
+      <form onSubmit={handleSubmit}>
+          <fieldset>
+            <div className='Feild'>
+              <label htmlFor='name'>Name: </label>
+              <input 
+              id='name'
+              type='text' 
+              placeholder='Name' name='name' 
+              value={name} 
+              onChange={(e) => setName(e.target.value)}/>
+            </div>
+            <button disabled={!name}type='submit'>Submit</button>
+          </fieldset>
+        </form>
     </div>
   );
   
